eb5146ce3d9c1fb6d3a6f2e224935a8af03b7031
afrinshaaslam <afrinshaaslam0671@gmail.com>
2024-05-05 18:44:09 +0530

    initial push

---
+++ assets/js/site.js
@@ -0,0 +1,65 @@
+"use strict";
+
+const toggleThemeBtn = document.querySelector("#toggleTheme");
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+const nav = document.querySelector(".nav-bar");
+const header = document.querySelector(".header");
+const navLogo = document.querySelector(".nav-logo");
+
+toggleThemeBtn.addEventListener("click", function () {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  const lightThemeIcon = document.querySelector("#lightThemeIcon");
+  const dataThemeIcon = document.querySelector("#dataThemeIcon");
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+
+  const logoHTML =
+    currentTheme === "light"
+      ? `<img class="nav-logo-img" src="assets/image/ZL-LOGO-white.png" alt="Zigma-lab-logo" />`
+      : `<img class="nav-logo-img" src="assets/image/ZL-LOGO.png" alt="Zigma-lab-logo" />`;
+
+  navLogo.innerHTML = logoHTML;
+
+  document.documentElement.setAttribute("data-theme", newTheme);
+  lightThemeIcon.classList.toggle("hide");
+  dataThemeIcon.classList.toggle("hide");
+});
+
+window.addEventListener("mousemove", function (e) {
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.animate(
+    {
+      left: `${posX}px`,
+      top: `${posY}px`,
+    },
+    { duration: 100, fill: "forwards" }
+  );
+
+  cursorOutline.animate(
+    {
+      left: `${posX}px`,
+      top: `${posY}px`,
+    },
+    { duration: 700, fill: "forwards" }
+  );
+});
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+};
+
+const obsObject = {
+  root: null,
+  threshold: 0,
+  rootMargin: "-90px",
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, obsObject);
+headerObserver.observe(header);
