913a41fc41bb8ef4f1762aad71d00754e7df8c61
afrinshaaslam <afrinshaaslam0671@gmail.com>
2024-05-07 21:14:47 +0530

    more changes

--- assets/js/site.js
+++ assets/js/site.js
@@ -15,8 +15,8 @@ toggleThemeBtn.addEventListener("click", function () {
 
   const logoHTML =
     currentTheme === "light"
-      ? `<img class="nav-logo-img" src="assets/image/elephandora white.jpg" alt="elephandora-logo" />`
-      : `<img class="nav-logo-img" src="assets/image/elephandora black.jpg" alt="elephandora-logo" />`;
+      ? `<img class="nav-logo-img" src="assets/image/elephandora white logo.png" alt="elephandora-logo" />`
+      : `<img class="nav-logo-img" src="assets/image/elephandora black logo.png" alt="elephandora-logo" />`;
 
   navLogo.innerHTML = logoHTML;
 
